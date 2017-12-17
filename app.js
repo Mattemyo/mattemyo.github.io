87713ef6cb8b32c807c63749e1e74f09faff96e8
Mattemyo <matias97marti@gmail.com>
2017-12-17 23:45:50 +0100

    "Start About Page With Images"

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ $(function () {
     $('body').append($background);
     $background = $('span')[0];
     console.log($background);
-    $('a').addClass('initial-color')
+    $('a').addClass('initial-color');
 
     function changeBackground() {
         //remove all classes from a
-        // $('a').removeClass();
-        // $('a').addClass('initial-color');
+        $('a').removeClass();
+        $('a').addClass('initial-color');
 
         const gCoords = this.getBoundingClientRect();
         // console.log(gCoords);
@@ -21,25 +21,29 @@ $(function () {
             top: gCoords.top + window.scrollY,
             left: gCoords.left + window.scrollX
         };
-
-        console.log(coord.height);
-        console.log($(this).css('background'));
-
-
-        //values of span depends on span that is hovered
+        //background value changes
         $background.style.width = `${coord.width}px`;
         $background.style.height = `${coord.height}px`;
         $background.style.transform = `translate(${coord.left}px, ${coord.top}px`;
         $background.style.background = $('.initial-color').css('color');
         $background.style.border = $('.initial-color').css('color');
+        $background.style.position = 'absolute';
 
         //change font color of a
-        $(this).addClass('inverse-color');
+        $(this).css('color' , '$darkgreen !important');
     }
     function getInitialClass() {
         $('a').removeClass();
         $('a').addClass('initial-color');
     }
-    $hovered.mouseleave(getInitialClass);
-    $hovered.mouseenter(getInitialClass, changeBackground);
+    $hovered.mouseover(changeBackground);
+    $background.scroll(function(){
+        $background.css('display', 'none');
+    });
+//about
+
+
+//big Images
+$('.bigImg').addClass('col-12');
+
 });
