13a1b028989a1548f12d290baaf24c9bc5905fce
Mattemyo <matias97marti@gmail.com>
2017-12-19 00:01:37 +0100

    Slideshow with cubic-bezier

--- app.js
+++ app.js
@@ -1,55 +1,60 @@
-$(function () {
-    const $hovered = $('a');
-    var $background = '<span class="background"></span>';
-    $('body').append($background);
-    $background = $('span')[0];
-    console.log($background);
-    $('a').addClass('initial-color');
+$(function() {
+  const $hovered = $("a");
+  var $background = '<span class="background"></span>';
+  $("body").append($background);
+  $background = $("span")[0];
+  console.log($background);
+  $("a").addClass("initial-color");
 
-    function changeBackground() {
-        //remove all classes from a
-        $('a').removeClass();
-        $('a').addClass('initial-color');
+  function changeBackground() {
+    //remove all classes from a
+    $("a").removeClass();
+    $("a").addClass("initial-color");
 
-        const gCoords = this.getBoundingClientRect();
-        // console.log(gCoords);
-        let headerHeight = $('header')[0];
+    const gCoords = this.getBoundingClientRect();
+    // console.log(gCoords);
+    let headerHeight = $("header")[0];
 
-        let coord = {
-            width: gCoords.width,
-            height: gCoords.height,
-            top: gCoords.top + window.scrollY,
-            left: gCoords.left + window.scrollX
-        };
-        //background value changes
-        $background.style.width = `${coord.width}px`;
-        $background.style.height = `${coord.height}px`;
-        $background.style.transform = `translate(${coord.left}px, ${coord.top}px`;
-        $background.style.background = $('.initial-color').css('color');
-        $background.style.border = $('.initial-color').css('color');
-        $background.style.position = 'absolute';
+    let coord = {
+      width: gCoords.width,
+      height: gCoords.height,
+      top: gCoords.top + window.scrollY,
+      left: gCoords.left + window.scrollX
+    };
+    //background value changes
+    $background.style.width = `${coord.width}px`;
+    $background.style.height = `${coord.height}px`;
+    $background.style.transform = `translate(${coord.left}px, ${coord.top}px`;
+    $background.style.background = $(".initial-color").css("color");
+    $background.style.border = $(".initial-color").css("color");
+    $background.style.position = "absolute";
 
-        //change font color of a
-        $(this).css('color' , '$darkgreen !important');
-    }
-    function getInitialClass() {
-        $('a').removeClass();
-        $('a').addClass('initial-color');
-    }
-    $hovered.mouseover(changeBackground);
-    $background.scroll(function(){
-        $background.css('display', 'none');
-    });
-//about
+    //change font color of a
+    $(this).css("color", "$darkgreen !important");
+  }
+  function getInitialClass() {
+    $("a").removeClass();
+    $("a").addClass("initial-color");
+  }
+  $hovered.mouseover(changeBackground);
 
+  //make span sticky just as navbar
 
-//big Images slides
-let index = 0;
-startSlides();
-function startSlides(){
+  //about
 
-    
+  //big Images slides
+  const $figure = $("figure")[0];
+  console.log($figure);
+  figure = document.querySelector("figure");
 
-}
+  //total number of % that has been translated
+  let total = 0;
+  window.setInterval(() => {
+      //20% decrease in translateX for every slide 
+    total -= 20;
+    figure.style.transform = `translateX(${total}%)`;
+  }, 5000);
 
+  startSlides();
+  function startSlides() {}
 });
