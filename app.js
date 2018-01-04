29550bc901c83aa59013434d2e211aa55ec04b3f
Mattemyo <matias97marti@gmail.com>
2018-01-04 22:45:11 +0100

    Delete sass and map files, add smooth scroll

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 $(function() {
+  //HIGHLIGHT
   const $hovered = $("a");
   var $background = '<span class="background"></span>';
-  $("body").append($background);
+  $("nav").append($background);
   $background = $("span")[0];
   console.log($background);
   $("a").addClass("initial-color");
@@ -12,7 +13,6 @@ $(function() {
     $("a").addClass("initial-color");
 
     const gCoords = this.getBoundingClientRect();
-    // console.log(gCoords);
     let headerHeight = $("header")[0];
 
     let coord = {
@@ -24,22 +24,16 @@ $(function() {
     //background value changes
     $background.style.width = `${coord.width}px`;
     $background.style.height = `${coord.height}px`;
-    $background.style.transform = `translate(${coord.left}px, ${coord.top}px`;
+    $background.style.transform = `translate(${coord.left}px`;
+    //add class
+
     $background.style.background = $(".initial-color").css("color");
-    $background.style.border = $(".initial-color").css("color");
     $background.style.position = "absolute";
-
-    //change font color of a
-    $(this).css("color", "$darkgreen !important");
   }
- 
+
   $hovered.mouseover(changeBackground);
-//TODO: scrollY. add class sticky
-  //make span sticky just as navbar
 
-  //about
-
-  //big Images slides
+  //SLIDES
   const $figure = $("figure")[0];
   console.log($figure);
   figure = document.querySelector("figure");
@@ -47,11 +41,46 @@ $(function() {
   //total number of % that has been translated
   let total = 0;
   window.setInterval(() => {
-      //20% decrease in translateX for every slide 
+    //20% decrease in translateX for every slide
     total -= 20;
     figure.style.transform = `translateX(${total}%)`;
   }, 5000);
+  //ADD NICE SCROLL
 
-  startSlides();
-  function startSlides() {}
+  $("ul a").on("click", function(e) {
+    e.preventDefault();
+    $("html, body").stop();
+    //href in navbar refers to id of container
+    const id = $(this).attr("href");
+    //position of container
+    const finishOffset = $(id).offset().top;
+    //offset of current window
+    const windowOffset = $(window).scrollTop();
+    //speed depends on distance between current and final position
+    const diff = Math.abs(finishOffset - windowOffset);
+
+    let bounce;
+    if (diff < 200) {
+      bounce = 1;
+    } else if (windowOffset < finishOffset) {
+      bounce = 10;
+    } else {
+      bounce = -10;
+    }
+
+    console.log(finishOffset);
+    $("html, body")
+      .animate(
+        {
+          scrollTop: finishOffset + bounce
+        },
+        diff * 2
+      )
+      .animate(
+        {
+          scrollTop: finishOffset - bounce
+        },
+        0.8*diff
+      );
+  });
 });
